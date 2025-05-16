f5a2042c6f0534284bd39e84a800397ea169f2c9
Torok Donat <td.zazsa@gmail.com>
2025-05-16 11:34:29 +0200

    Játékmenet szépítése

--- Jatek.cs
+++ Jatek.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace legyenOnIsMilliomos
@@ -21,21 +22,45 @@ namespace legyenOnIsMilliomos
 
         public void JatekIndit()
         {
+            Console.WriteLine("Üdvözlöm a Legyen Ön is milliomos játékban!");
+            Thread.Sleep(1000);
+            Console.WriteLine("Itt az idő megválaszolni a sorkérdést!");
+            Thread.Sleep(1000);
+
             if (SorKerdesJatszas())
             {
+                Console.Clear();
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("A válasza helyes volt!");
+                Console.ResetColor();
+                Thread.Sleep(1000);
+                Console.WriteLine("Tovább mehet a rendes kérdésekre ahol már pénzt is nyerhet!");
+                Thread.Sleep(2500);
+
                 Kerdes k = new Kerdes();
 
                 int lepes = 0;
                 bool marade = true;
                 while (marade)
                 {
+                    Console.ForegroundColor = ConsoleColor.Magenta;
+                    Console.WriteLine((lepes+1)+". kör következik");
+                    Console.ResetColor ();
+                    Thread.Sleep(1000);
+                    
                     k.SorKerdesHuzas(lepes);
                     Console.WriteLine("Jelenlegi egyenleg: "+penzosszeg+" Ft");
                     k.SorKerdesKiiras();
 
+                    Console.ForegroundColor = ConsoleColor.Cyan;
                     if (k.ValaszCheck(Console.ReadLine()))
                     {
-                        Console.WriteLine("Helyes válasz!");
+                        Console.Clear();
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine("A válasza helyes volt!");
+                        Console.ResetColor();
+                        Thread.Sleep(1000);
+                        Console.Clear();
                         penzosszeg *= 2;
                         if (penzosszeg == 0)
                         {
